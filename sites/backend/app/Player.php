98fc4cbc67e43352bd587cd958a28b614d46cd89
Jericho <sprobe.engineer@gmail.com>
2020-04-19 16:54:44 +0900

    Add Player model.

---
+++ sites/backend/app/Player.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use App\Models\BaseModel;
+
+class Player extends BaseModel
+{
+    protected $table = 'players';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $fillable = ['name', 'email', 'phone_number', 'password'];
+
+    /**
+     * @var int Maximum acceptable length of the name attribute
+     */
+    const NAME_MAX_LENGTH = 20;
+    /**
+     * @var int Maximum acceptable length of the email attribute
+     */
+    const EMAIL_MAX_LENGTH = 100;
+    /**
+     * @var int Maximum acceptable length of the phone number attribute
+     */
+    const PHONE_NUMBER_MAX_LENGTH = 11;
+
+    public function gamesCreated()
+    {
+        return $this->hasMany(Game::class, 'created_by', 'id');
+    }
+
+    public function teams()
+    {
+        // TODO Add relation
+    }
+
+    public function activities()
+    {
+        // TODO Add relation
+    }
+
+    public function gameMasterOfGames()
+    {
+        return $this->hasMany(GameTeam::class, 'game_master', 'id');
+    }
+}
