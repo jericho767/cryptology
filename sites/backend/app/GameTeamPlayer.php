55437f1955ba2f8427d891dee34057f14f5b664d
Jericho <sprobe.engineer@gmail.com>
2020-04-19 17:07:00 +0900

    Add GameTeamPlayer model.

---
+++ sites/backend/app/GameTeamPlayer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use App\Models\BaseModel;
+
+class GameTeamPlayer extends BaseModel
+{
+    protected $table = 'game_team_players';
+    protected $primaryKey = ['game_team_id', 'player_id'];
+    public $incrementing = false;
+    protected $fillable = ['game_team_id', 'player_id'];
+
+    public function gameTeam()
+    {
+        return $this->belongsTo(GameTeam::class, 'game_team_id', 'id');
+    }
+
+    public function player()
+    {
+        return $this->belongsTo(Player::class, 'player_id', 'id');
+    }
+
+    public function turnOrder()
+    {
+        // TODO Define
+    }
+}
