973b1afb648277d46019b16ff441c2d539a9d37b
Jericho <sprobe.engineer@gmail.com>
2020-04-19 19:00:49 +0900

    Add foreign key constraint for `game_team_player_id` in `turn_orders` table.

---
+++ sites/backend/database/migrations/2020_04_03_145238_create_fk_turn_orders_game_team_player_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFkTurnOrdersGameTeamPlayerId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('turn_orders', function (Blueprint $table) {
+            $table->foreign('game_team_player_id')
+                ->references('id')
+                ->on('game_team_players');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('turn_orders', function (Blueprint $table) {
+            $table->dropForeign('turn_orders_game_team_player_id_foreign');
+        });
+    }
+}
