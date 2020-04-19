73ceff977d50bb43b59f79a367b7ca15090090e0
Jericho <sprobe.engineer@gmail.com>
2020-04-19 16:02:43 +0900

    Add GameTeam model.

---
+++ sites/backend/app/GameTeam.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use App\Models\BaseModel;
+
+class GameTeam extends BaseModel
+{
+    protected $table = 'game_teams';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $fillable = ['game_id', 'team_name', 'game_master'];
+
+    public function gameWon()
+    {
+        return $this->hasOne(Game::class, 'winner', 'id');
+    }
+
+    public function gameBlocks()
+    {
+        return $this->hasMany(GameMap::class, 'game_team_id', 'id');
+    }
+
+    public function gameMaster()
+    {
+        // TODO Add relation
+    }
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class, 'game_id', 'id');
+    }
+
+    public function players()
+    {
+        // TODO Add relation
+    }
+}
