9a7d3675bcb907835c5f6de49f87e5812ea92c2f
Jericho <sprobe.engineer@gmail.com>
2020-04-19 19:05:03 +0900

    Add foreign key constraint for `game_turn_id` in `turn_guesses` table.

---
+++ sites/backend/database/migrations/2020_04_03_151112_create_fk_turn_guesses_game_turn_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFkTurnGuessesGameTurnId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('turn_guesses', function (Blueprint $table) {
+            $table->foreign('game_turn_id')
+                ->references('id')
+                ->on('game_turns');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('turn_guesses', function (Blueprint $table) {
+            $table->dropForeign('turn_guesses_game_turn_id_foreign');
+        });
+    }
+}
