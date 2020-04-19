117eda0965e85803026828c3a799e81f792ffdfb
Jericho <sprobe.engineer@gmail.com>
2020-04-19 18:24:34 +0900

    Create migration for `game_turns` table.

---
+++ sites/backend/database/migrations/2020_04_02_113225_create_game_turns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameTurnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_turns', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('turn_order_id');
+            $table->unsignedSmallInteger('guess_count');
+            $table->string('clue');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_turns');
+    }
+}
