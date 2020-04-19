6c940faaced65ac197bfd5726138ad3a0940b969
Jericho <sprobe.engineer@gmail.com>
2020-04-19 18:20:11 +0900

    Create migration for `turn_orders` table.

---
+++ sites/backend/database/migrations/2020_04_02_112430_create_turn_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTurnOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turn_orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('id', true)->unique();
+            $table->unsignedBigInteger('game_team_player_id')->primary();
+            $table->unsignedTinyInteger('has_played')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('turn_orders');
+    }
+}
