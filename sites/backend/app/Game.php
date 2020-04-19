0e2b62132a55ea0be268d6ca1f16a07339a7165c
Jericho <sprobe.engineer@gmail.com>
2020-04-19 15:11:42 +0900

    Add Game model.

---
+++ sites/backend/app/Game.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use App\Models\BaseModel;
+
+class Game extends BaseModel
+{
+    protected $table = 'games';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $fillable = ['name', 'winner', 'created_by'];
+
+    /**
+     * @var int Maximum acceptable length of the name attribute
+     */
+    const NAME_MAX_LENGTH = 20;
+    /**
+     * @var int Number of teams to be playing in a game
+     */
+    const MAX_NUMBER_OF_TEAMS = 2;
+    /**
+     * @var int Minimum number of players per team in order to play
+     */
+    const MIN_PLAYERS_PER_TEAM = 2;
+
+    public function mapBlocks()
+    {
+        return $this->hasMany(GameMap::class, 'game_id', 'id');
+    }
+
+    public function winner()
+    {
+        // TODO Add relation
+    }
+
+    public function participants()
+    {
+        // TODO Add relation
+    }
+
+    public function creator()
+    {
+        // TODO Add relation
+    }
+}
