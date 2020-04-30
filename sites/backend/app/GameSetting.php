ccd0ec355ddfb3b6b84e40431333b14e037bdf82
Jericho <sprobe.engineer@gmail.com>
2020-05-01 00:44:15 +0900

    Add validation before saving GameSetting model.

--- sites/backend/app/GameSetting.php
+++ sites/backend/app/GameSetting.php
@@ -16,4 +16,62 @@ class GameSetting extends BaseModel
         'min_players', // Minimum number of players that a team can have in order to play
         'max_players', // Maximum number of players that a team can have in order to play
     ];
+
+    /**
+     * Validates the model.
+     *
+     * @return string|null
+     */
+    protected function validate(): ?string
+    {
+        $mapSize = $this->getAttribute('map_size');
+        $guessCount = $this->getAttribute('guess_count');
+        $maxTeams = $this->getAttribute('max_teams');
+        $minPlayers = $this->getAttribute('min_players');
+        $maxPlayers = $this->getAttribute('max_players');
+
+        if ($guessCount < 1) {
+            return 'Guess count cannot be zero.';
+        }
+
+        // 2 is the least number of teams to operate a game
+        if ($maxTeams < 2) {
+            return 'Team count must be at least 2.';
+        }
+
+        // 2 is the least number of players for a team to play (1 for guesser, 1 for game master)
+        if ($minPlayers < 2 || $maxPlayers < 2) {
+            return 'Player count needs to be at least 2.';
+        }
+
+        $roleBlocks = 0;
+        // Additional blocks for each team
+        for ($i = 0; $i < $maxTeams; $i++) {
+            // $i - count of blocks for additional guess for each team
+            $roleBlocks += $i;
+        }
+
+        // Number of assassins
+        $assassinsCount = $maxTeams - 1;
+        $roleBlocks += $guessCount * $maxTeams + $assassinsCount;
+
+        if ($mapSize <= $roleBlocks) {
+            return 'Cannot fix number of role blocks in the map.';
+        }
+
+        return null;
+    }
+
+    public static function boot(): void
+    {
+        parent::boot();
+
+        self::creating(function(GameSetting $model): bool {
+            return $model->validate() === null;
+        });
+
+        self::updating(function(GameSetting $model): bool {
+            return $model->validate() === null;
+        });
+    }
 }
