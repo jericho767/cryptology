19ef2b33fde0a459f85da61acd4b47787c73efac
Jericho <sprobe.engineer@gmail.com>
2020-05-01 17:49:04 +0900

    Add `game_settings` seeder.

---
+++ sites/backend/database/seeds/GameSettingSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\GameSetting;
+use Illuminate\Database\Seeder;
+
+class GameSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $gameSetting = new GameSetting([
+            'map_size' => 25,
+            'guess_count' => 8,
+            'max_teams' => 5,
+            'min_players' => 2,
+            'max_players' => 6,
+        ]);
+        $gameSetting->save();
+    }
+}
