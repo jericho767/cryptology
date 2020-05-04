6664bf5bf646a7809dc9c5aefaf80f629e642b95
Jericho <sprobe.engineer@gmail.com>
2020-05-04 23:58:48 +0900

    Implement to check if the game is running.

--- sites/backend/app/Services/GameService.php
+++ sites/backend/app/Services/GameService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\Game;
+use Illuminate\Database\Eloquent\Builder;
+
 class GameService extends BaseService
 {
     public function __construct()
@@ -18,5 +21,23 @@ class GameService extends BaseService
     public function getNumOfAssassins(int $participantCount): int
     {
         return $participantCount - 1;
+    }
+
+    /**
+     * Check if the game is still running given a `game_teams.id` value
+     *
+     * @param int $gameTeamId
+     * @return bool
+     */
+    public function isGameRunningByTeamId(int $gameTeamId): bool
+    {
+        $game = Game::query()
+            ->whereHas('participants', function (Builder $builder) use ($gameTeamId): void {
+                $builder->where('id', $gameTeamId);
+            })
+            ->firstOrFail();
+
+        // Game is still running when the winner value is still `null`
+        return $game->getAttribute('winner') === null;
     }
 }
