52d623c53fb687475fe7857d2894cd2085a714c9
Jericho <sprobe.engineer@gmail.com>
2020-05-08 00:24:53 +0900

    Create method to set winner if possible.

--- sites/backend/app/Services/GameService.php
+++ sites/backend/app/Services/GameService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Models\Game;
-use Illuminate\Database\Eloquent\Builder;
+use App\Models\GameMap;
+use App\Models\GameTeam;
+use Illuminate\Support\Collection;
 
 /**
  * Class GameService
@@ -28,33 +30,87 @@ class GameService extends BaseService
     }
 
     /**
-     * Check if the game is still running given a `game_teams.id` value
+     * Checks the parameters if the game should have a winner
      *
-     * @param int $gameId
-     * @return bool
+     * @param $gameId
+     * @return int|null game_team id of the winner
      */
-    public function isGameRunning(int $gameId): bool
+    public function updateGameWinner($gameId): ?int
     {
-        $game = Game::query()->where('id', $gameId)->firstOrFail();
+        // Fetch the game
+        $game = Game::query()->find($gameId);
+        // Gets all the blocks of the game
+        $blocks = GameMap::with(['guessedAt.gameTurn.turnOrder.gameTeamPlayer'])
+            ->where('game_id', $gameId)
+            ->get();
+        // Gets all the participants of the game
+        $gameTeamIds = GameTeam::query()
+            ->where('game_id', $gameId)
+            ->get()
+            ->pluck('id');
+        // Check if all the assassins has been guessed
+        $hasUnGuessedAssassin = $blocks
+            ->filter(function (GameMap $block) {
+                $isGuessed = $block->getRelation('guessedAt') !== null;
+                $isAssassin = $block->getAttribute('block_owner') === GameMap::ASSASSIN_BLOCK_NUM;
 
-        // Game is still running when the winner value is still `null`
-        return $game->getAttribute('winner') === null;
-    }
+                return !$isGuessed && $isAssassin;
+            })->count() > 0;
+        // Contains the game team id of the winner
+        $winnerId = null;
 
-    /**
-     * Check if the given `$gameTeamId` is from `$gameId`
-     *
-     * @param int $gameTeamId
-     * @param int $gameId
-     * @return bool
-     */
-    public function isValidParticipant(int $gameTeamId, int $gameId): bool
-    {
-        return Game::query()
-            ->whereHas('participants', function (Builder $builder) use ($gameTeamId): void {
-                $builder->where('id', $gameTeamId);
-            })
-            ->where('id', $gameId)
-            ->exists();
+        // All assassins has been guessed
+        if (!$hasUnGuessedAssassin) {
+            $loserIds = $blocks
+                ->where('block_owner', GameMap::ASSASSIN_BLOCK_NUM)
+                ->transform(function (GameMap $gameMap) {
+                    return [
+                        'game_team_id' => $gameMap
+                            ->getRelation('guessedAt') // Gets the TurnGuess
+                            ->getRelation('gameTurn') // Gets which turn
+                            ->getRelation('turnOrder') // Gets the TurnOrder
+                            ->getRelation('gameTeamPlayer') // Gets the player of the turn
+                            ->getAttribute('game_team_id') // Get the `game_team_id`
+                    ];
+                })
+                ->pluck('game_team_id')
+                ->values();
+
+            // Fetch the winner by filtering out the losers
+            $winnerId = $gameTeamIds->filter(function (int $value) use ($loserIds) {
+                    return !$loserIds->containsStrict($value);
+                })
+                ->first();
+        } else {
+            // Fetch all team blocks
+            $teamBlocks = $blocks
+                ->whereNotNull('game_team_id')
+                ->groupBy('game_team_id');
+
+            // Look for the winner
+            $teamBlocks->each(function (Collection $blocks, $blockTeamIdOwner) use ($game, &$winnerId): bool {
+                $unguessedBlocks = $blocks->whereNull('guessedAt');
+
+                // All blocks have been guessed
+                if ($unguessedBlocks->count() === 0) {
+                    // We have a winner
+                    $winnerId = $blockTeamIdOwner;
+
+                    // End search of winner
+                    return false;
+                }
+
+                // Look again for the winner
+                return true;
+            });
+        }
+
+        // Check if there's a winner
+        if ($winnerId !== null) {
+            $game->setAttribute('winner', $winnerId);
+            $game->save();
+        }
+
+        return $winnerId;
     }
 }
