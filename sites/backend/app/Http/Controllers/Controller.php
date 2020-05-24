cc5e8304e07547d4dab428fb61e9da687e5794e5
Jericho <sprobe.engineer@gmail.com>
2020-05-24 19:40:54 +0900

    Add logged in user as class attribute to controller.

--- sites/backend/app/Http/Controllers/Controller.php
+++ sites/backend/app/Http/Controllers/Controller.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Logged in user
+     *
+     * @var Player
+     */
+    protected $user;
+
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::user();
+
+            return $next($request);
+        });
+    }
 }
