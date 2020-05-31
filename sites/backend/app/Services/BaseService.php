a275ecd976e0c228f2b3f74bf6570bf27031576e
Jericho <sprobe.engineer@gmail.com>
2020-05-31 23:25:48 +0900

    Add the logged in user as property to the service classes.

--- sites/backend/app/Services/BaseService.php
+++ sites/backend/app/Services/BaseService.php
@@ -2,14 +2,24 @@
 
 namespace App\Services;
 
+use App\Models\Player;
+use Illuminate\Support\Facades\Auth;
+
 /**
  * Class BaseService
  * @package App\Services
  */
 class BaseService
 {
+    /**
+     * Logged in player
+     *
+     * @var Player
+     */
+    protected $player;
+
     public function __construct()
     {
-
+        $this->player = Auth::user();
     }
 }
