bc580b87d516582e96b3256c28c70a94bde7e3df
Jericho <sprobe.engineer@gmail.com>
2020-06-03 21:37:49 +0900

    Create localization for error messages.

---
+++ sites/backend/resources/lang/en/errors.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Errors Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used as exception messages
+    | and explicitly defined error messages.
+    |
+    */
+
+    'permission' => [
+        'notAllowed' => 'You just cant cunt.',
+        'deny' => 'You can\'t touch this shit.',
+    ],
+    'request' => [
+        'jsonOnly' => 'I only answer to JSON requests bitch.',
+    ],
+    'param' => [
+        'incomplete' => 'Get the fuck out of here.',
+        'mismatch' => 'You lost motherfucker? Get your ass off here.',
+    ],
+    'game' => [
+        'doesNotExists' => 'Let me come with you, you little shit. Let\'s look for it together.',
+        'notRunning' => 'The fuck you trying to do? It is over you idiot. You\'re late.',
+    ],
+
+];
