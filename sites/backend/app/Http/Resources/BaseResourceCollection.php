c115cdd5a65dd79c78aa400209a80617d998a17a
Jericho <sprobe.engineer@gmail.com>
2020-06-03 22:47:31 +0900

    Create BaseResourceCollection class.

---
+++ sites/backend/app/Http/Resources/BaseResourceCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+/**
+ * Class BaseResourceCollection
+ * @package App\Http\Resources
+ */
+class BaseResourceCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection
+        ];
+    }
+}
--- sites/backend/app/Http/Resources/GameSettingCollection.php
+++ sites/backend/app/Http/Resources/GameSettingCollection.php
@@ -2,25 +2,11 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
 /**
  * Class GameSettingCollection
  * @package App\Http\Resources
  */
-class GameSettingCollection extends ResourceCollection
+class GameSettingCollection extends BaseResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param Request $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return [
-            'data' => $this->collection
-        ];
-    }
+
 }
