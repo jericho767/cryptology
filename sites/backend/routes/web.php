93781fbea5b4278f2f65e4f98dc8e94d1a5e473a
Jericho <sprobe.engineer@gmail.com>
2020-06-06 19:27:29 +0900

    Remove unused web route.

--- sites/backend/routes/web.php
+++ sites/backend/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +10,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-});
